90c0b6c39c8f6495f87860c8935267e7abbfba8e
b1mind <brent@1mindsolutions.com>
2023-09-15 13:02:56 -0600

    🧪 testing svelte-tools

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,13 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	preprocess: [vitePreprocess()]
+	preprocess: [vitePreprocess()],
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: 'alt-shift',
+			holdMode: false
+		}
+	}
 };
 
 export default config;
